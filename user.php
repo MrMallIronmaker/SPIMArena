f6c92e3e30a01711b8c05564944fcf9322bfcaaa
MrMallIronmaker <725mrm@gmail.com>
2015-05-17 01:21:01 -0500

    Create user.php
    Short file for getting the netID, will have more work later.

---
+++ user.php
@@ -0,0 +1,4 @@
+// is it bad to to have only one line of code?
+// I don't think so because there will be more useful tools in here soon.
+
+$netId = substr($_SERVER["eppn"], 0, strpos($_SERVER["eppn"], '@'));
