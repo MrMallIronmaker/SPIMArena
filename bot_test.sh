f55e51034550b00ee9387c3242b6d6a9b59d8bef
MrMallIronmaker <725mrm@gmail.com>
2015-05-21 18:26:53 -0500

    began to comment and nicify the code for the shell scripts (because that's the ugliest)

--- bot_test.sh
+++ bot_test.sh
@@ -10,19 +10,32 @@
 # the process end at about 8s. If they're the same, then it broke somewhere
 # before the 6s mark.
 
+# startup an Xvfb server in the background
+# this errors out if it's already created.
 Xvfb :34 &
 
+# the first argument is the bot file name
 BOT=$1
 
-#timeout 8s env DISPLAY=:67 $(dirname $0)/QtSpimbot -file $(dirname $0)/pending/$BOT -maponly -run -tournament -randommap -largemap -exit_when_done -multi_quadrants -quadrant_modes > $(dirname $0)/test_bot.txt 2>stderr.txt &
-timeout 8s env DISPLAY=:34 $(dirname $0)/QtSpimbot -file $(dirname $0)/pending/$BOT -debug -maponly -run -tournament -exit_when_done > $(dirname $0)/test_bot.txt 2>stderr.txt &
+# env DISPLAY=:34 sets the display to the :34 (the first argument to Xvfb)
+# most of the other calls are QtSpimbot specifics, 
+# so you should know them or get to know them
+timeout 8s env DISPLAY=:34 $(dirname $0)/QtSpimbot -file \
+$(dirname $0)/pending/$BOT -debug -maponly -run -tournament -exit_when_done \
+> $(dirname $0)/bot_test_out.txt 2>bot_test_err.txt &
+
+# let QtSpimbot run for six seconds, then get the output
 sleep 6s
-SIX=$(cat $(dirname $0)/test_bot.txt)
-wait $! # waits until the most recent background command returns (which is QtSpimbot)
-EIGHT=$(cat $(dirname $0)/test_bot.txt)
+SIX=$(cat $(dirname $0)/bot_test_out.txt)
+
+# wait until the most recent background command returns (which is QtSpimbot)
+wait $!
+EIGHT=$(cat $(dirname $0)/bot_test_out.txt)
+
+# move or delete depending if the bot passed
 if [ "$SIX" != "$EIGHT" ]
-then
+then # passed test
 	mv $(dirname $0)/pending/$BOT $(dirname $0)/bots
-else
+else # failed test
 	rm $(dirname $0)/pending/$BOT
 fi
