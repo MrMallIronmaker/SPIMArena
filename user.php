3ce8f5829a92fce91320cbfc7d1b5f2ed32bb9e1
MrMallIronmaker <725mrm@gmail.com>
2015-07-22 19:37:59 -0500

    Updating words feature

--- user.php
+++ user.php
@@ -1,5 +1,11 @@
 <?php
-	// is it bad to to have only one line of code?
-	// I don't think so because there will be more useful tools in here soon.
+	// get netid
 	$netId = substr($_SERVER["eppn"], 0, strpos($_SERVER["eppn"], '@'));
+	// get hash_name from netid
+	$words_file = file("words");
+	$binary = hash("md5", $netId, TRUE);
+	$dec = unpack('L', $binary)[1];
+	$hash1 = $dec % 1024;
+	$hash2 = ($dec / 1024) % 1024;
+	$hash_name = $words_file[$hash1 + 1] . $words_file[$hash2 + 1];
 ?>
